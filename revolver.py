8b3fe2ad8d430b77c25172379de74f4c2058b843
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-06-24 12:15:48 +0800

    I ADDED A FAIR GAME SO THAT IT WONT WEAR OUT YOUR LAPTOP JUST LIKE MINE! I ALWAYS LOSE EVEN IF THE AI WON! so i implemented that it will only shutdown from the player's control not even the AI itself to be fair (i counted 12 shutdowns i faced during testing my program)

--- revolver.py
+++ revolver.py
@@ -17,16 +17,16 @@ class RussianRouletteGame:
         if self.current_chamber == self.bullet_position:
             player.kill()
             print("✴ BANG! They're dead.")
+ 
+            if player.__class__.__name__ != "AIEnemy":
+                os_name = platform.system()
+                if os_name == "Windows":
+                    os.system("shutdown /s /t 10")
+                elif os_name == "Linux" or os_name == "Darwin":
+                    os.system("shutdown -h now")
 
-            # Shutdown the device
-            os_name = platform.system()
-            if os_name == "Windows":
-                os.system("shutdown /s /t 5")
-            elif os_name == "Linux" or os_name == "Darwin":  # Darwin = macOS
-                os.system("shutdown -h now")
-
-            else:
-             print("CLICK!..... They're safe.")
-             self.current_chamber = (self.current_chamber % 6) + 1
+        else:
+            print("CLICK!..... They're safe.")
+            self.current_chamber = (self.current_chamber % 6) + 1
 
         self.spin_chamber()
